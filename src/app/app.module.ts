09d7f887049ae13f38a2379425a9a802b19d9bba
nitayneeman <nitayneeman@users.noreply.github.com>
2018-08-11 21:36:09 +0300

    Using the custom element

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { MadeWithLoveModule } from 'angular-made-with-love';
 
 import { AppComponent } from './app.component';
@@ -12,7 +12,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     MadeWithLoveModule
   ],
-  providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
